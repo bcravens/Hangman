d391000b2b2ee23f27f09d3fabcfedd1385b7c5d
Bradley Cravens <bradleycravens@gmail.com>
2016-10-14 13:21:05 -0400

    Letter a now appears if word contains it

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 $("document").ready(function(){
 var hangman = {
+  inputWord: [],
   wordUnderscore: [],
 };
 
   $("#entry").on("submit", function(pd){
     pd.preventDefault();
-    var userInput = $("#newEntry").val();
+    var userInput = $("#newEntry").val().toLowerCase();
     var changeInput = userInput.split('');
+    hangman.inputWord = changeInput.concat();
       for ( i=0; i<changeInput.length; i++) {
         hangman.wordUnderscore[i] = "_";
       }
@@ -15,6 +17,14 @@ var hangman = {
   });
 
   $("#a").on("click", function(){
+    if (hangman.inputWord.includes("a")) {
+      for (i=0; i<hangman.inputWord.length; i++) {
+        if (hangman.inputWord[i] === ("a")){
+          hangman.wordUnderscore[i] = "a";
+        }
+        $("#inputGoesHere").html(hangman.wordUnderscore);
+      }
+    }
   });
 
 
