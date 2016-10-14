f4e8ac9974ddd38aeb03f0d1dd0f269842921cca
Bradley Cravens <bradleycravens@gmail.com>
2016-10-14 13:53:12 -0400

    Now works for all letters

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ $("document").ready(function(){
 var hangman = {
   inputWord: [],
   wordUnderscore: [],
+  val: [],
 };
 
   $("#entry").on("submit", function(pd){
@@ -16,11 +17,18 @@ var hangman = {
     $("this").trigger("reset");
   });
 
-  $("#a").on("click", function(){
-    if (hangman.inputWord.includes("a")) {
+  $(".letter").on("click", function(pd){
+    pd.preventDefault();
+    var grabValue = this.value;
+    hangman.val = grabValue;
+    console.log(hangman.val);
+  });
+
+  $(".letter").on("click", function(){
+    if (hangman.inputWord.includes(hangman.val)) {
       for (i=0; i<hangman.inputWord.length; i++) {
-        if (hangman.inputWord[i] === ("a")){
-          hangman.wordUnderscore[i] = "a";
+        if (hangman.inputWord[i] === (hangman.val)){
+          hangman.wordUnderscore[i] = hangman.val;
         }
         $("#inputGoesHere").html(hangman.wordUnderscore);
       }
