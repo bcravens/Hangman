22afdcdc44c437caa0af5438b4fb463747493bda
Bradley Cravens <bradleycravens@gmail.com>
2016-10-13 20:25:33 -0400

    Buttons now check userInput to see if theres a matching letter

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 $("document").ready(function(){
   var entry = $("#entry");
+  //var letters = "abcdefghijklmnopqrstuvwxyz".split('');
 
   function grabWord () {
     entry.on("submit", function(pd){
@@ -8,8 +9,24 @@ $("document").ready(function(){
       var newEntry = $("#newEntry").val();
       $("#inputGoesHere").html(newEntry);
       entry.trigger("reset");
+      $("#inputGoesHere").hide();
     });
   }
+
+$("#a").on("click", function(){
+  var userInput = $("#inputGoesHere").text();
+  var regexp = /[a]/gi;
+  var matches_array = userInput.match(regexp);
+  console.log(matches_array);
+});
+
+$("#b").on("click", function(){
+  var userInput = $("#inputGoesHere").text();
+  var regexp = /[b]/gi;
+  var matches_array = userInput.match(regexp);
+  console.log(matches_array);
+});
+
 
 grabWord();
 });
