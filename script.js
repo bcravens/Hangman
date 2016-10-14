0049ce94280c6f21c62b8905214a7c678cb19050
Bradley Cravens <bradleycravens@gmail.com>
2016-10-14 10:59:22 -0400

    Puts user input into underscores and displays it

--- script.js
+++ script.js
@@ -1,32 +1,21 @@
 $("document").ready(function(){
-  var entry = $("#entry");
-  //var letters = "abcdefghijklmnopqrstuvwxyz".split('');
+var hangman = {
+  wordUnderscore: [],
+};
 
-  function grabWord () {
-    entry.on("submit", function(pd){
-      pd.preventDefault();
-      console.log($("#newEntry").val());
-      var newEntry = $("#newEntry").val();
-      $("#inputGoesHere").html(newEntry);
-      entry.trigger("reset");
-      $("#inputGoesHere").hide();
-    });
-  }
+  $("#entry").on("submit", function(pd){
+    pd.preventDefault();
+    var userInput = $("#newEntry").val();
+    var changeInput = userInput.split('');
+      for ( i=0; i<changeInput.length; i++) {
+        hangman.wordUnderscore[i] = "_";
+      }
+    $("#inputGoesHere").html(hangman.wordUnderscore);
+    $("this").trigger("reset");
+  });
 
-$("#a").on("click", function(){
-  var userInput = $("#inputGoesHere").text();
-  var regexp = /[a]/gi;
-  var matches_array = userInput.match(regexp);
-  console.log(matches_array);
-});
-
-$("#b").on("click", function(){
-  var userInput = $("#inputGoesHere").text();
-  var regexp = /[b]/gi;
-  var matches_array = userInput.match(regexp);
-  console.log(matches_array);
-});
+  $("#a").on("click", function(){
+  });
 
 
-grabWord();
 });
