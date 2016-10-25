16a82ded9dc4789d4f35bf15fc7210a03b5156e0
Andy <andrew.sunglae.kim@gmail.com>
2016-10-25 15:57:57 -0400

    adds comments/rubric

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 $("document").ready(function(){
+  // this object should be nested in the document ready like the rest of your code
 var hangman = {
   inputWord: [],
   wordUnderscore: [],
@@ -13,6 +14,7 @@ var hangman = {
   hideResult();
 
   $("#entry").on("submit", function(pd){
+    // these event listeners are getting pretty unwieldy, how can this be abstracted into more functions?
     pd.preventDefault();
     if (hangman.reset === 1) {
       alert("You must finish your current game first!");
@@ -29,6 +31,7 @@ var hangman = {
     hideBody();});
 
   $(".letter").on("click", function(){
+    // this will work, but i would encourage you to work with just jquery or just vanilla js, could potentially cause some issues
     hangman.val = this.value;
     if (hangman.inputWord.includes(hangman.val)) {
       for (i=0; i<hangman.inputWord.length; i++) {
@@ -41,6 +44,7 @@ var hangman = {
       }
     } else if ($("#guessedLetters").html().includes(hangman.val) && hangman.checkSubmit !== 0) {
       alert("You already guessed that letter.");
+      // this should definitely be abstracted into some more functions, mebbe draw hangman or something
     } else if (hangman.counter === 4) {
         $(hangman.bodyParts[5]).css('border','2px solid black');
         $("#lose").show();
@@ -62,6 +66,7 @@ var hangman = {
     for (i=0; i<hangman.bodyParts.length; i++)
       $(hangman.bodyParts[i]).css('border','none');}
 
+// clever!
   function checksForWin() {
     if (hangman.wordUnderscore.includes("_")) {
     } else {
@@ -69,6 +74,7 @@ var hangman = {
       hangman.win +=1;
       hangman.reset = 0;}}
 
+// this is a small detail, but maybe reset() instead of resets() In english if i'm resetting something i would say "reset this" not "resets this". In the same way, when we invoke a function we're doing that thing
   function resets(){
     hideResult();
     $("#guessedLetters").html(null);
@@ -82,14 +88,17 @@ var hangman = {
     hangman.wordUnderscore = [];
     $("#inputGoesHere").html(null);
     for (i=0; i<hangman.bodyParts.length; i++) {
-      $(hangman.bodyParts[i]).css('border','2px solid black');}}
+      $(hangman.bodyParts[i]).css('border','2px solid black');}} // put this curly brace on the next line for readability
 
+    // this might be intentional, but its slightly smelly. Whenever resets is called it then defines countdown, i would make this function outside of reset's definition
+    // also checkout setInterval, instead of this function below.
     function countdown(minutes) {
       var mins = minutes;
       var seconds = 60;
       function tick() {
         var current_minutes = mins-1;
         seconds--;
+        // heres you write String and also toString try to stay consistent, or is there a reason you opted for on or ther other?
         $("#counter").html(current_minutes.toString()+ ":" + (seconds < 10 ? "0" : "") + String(seconds));
         if (hangman.win == 1 || hangman.win == -1) {
           return;
